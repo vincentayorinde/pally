8ef70ba1a3fd88ad237f3513ff38f1d989e6096c
vincentayorinde <vincentayorinde@yahoo.com>
2022-07-12 18:03:57 +0100

    remove semi-colons

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ import cors from "cors"
 import { dirname, join } from "path"
 import { fileURLToPath } from "url"
 
-dotenv.config();
+dotenv.config()
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const app = express()
  
-const corsOptions = { credential: true, origin: process.env.URL || "*" };
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
+const corsOptions = { credential: true, origin: process.env.URL || "*" }
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
 
-app.use("/", express.static(join(__dirname, "public")));
+app.use("/", express.static(join(__dirname, "public")))
 
-export default app; 
+export default app
