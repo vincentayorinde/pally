388845b468c0af479ef7cc6fa25cec462917b8f8
vincentayorinde <vincentayorinde@yahoo.com>
2022-08-11 03:13:21 +0100

    test root server

--- app.test.js
+++ app.test.js
@@ -8,3 +8,11 @@ describe("setup test", () => {
     expect(user.test).toBe(user.test);
   });
 });
+
+describe("test root server endpoint", () => {
+    test("should be able to access server and return 200", async () => {
+        const response = await request(app).get("/").send();
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({});
+      });
+})
