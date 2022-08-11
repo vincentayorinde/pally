3275c16d79c0d1b3f97dd053274136892ca03bdf
vincentayorinde <vincentayorinde@yahoo.com>
2022-08-11 02:55:43 +0100

    setup testing

---
+++ app.test.js
@@ -0,0 +1,10 @@
+import request from "supertest";
+import app from "./app.js";
+import fs from "fs";
+
+describe("setup test", () => {
+  test("should ensure testing works!", async () => {
+    const user = { test: "testing works!" };
+    expect(user.test).toBe(user.test);
+  });
+});
