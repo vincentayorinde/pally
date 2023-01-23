0c0bdf8494ce0621389921b50bcc339802ea2b70
vincentayorinde <vincentayorinde@yahoo.com>
2023-01-23 00:34:36 +0000

    add sequelize cjs

---
+++ db/config/config.cjs
@@ -0,0 +1,25 @@
+const config = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DEV_DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.TEST_DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+    },
+}
+
+module.exports = config
---
+++ db/models/User.cjs
@@ -0,0 +1,21 @@
+const User = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: false,
+            },
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: false,
+            },
+        },
+    })
+    return User
+}
+
+module.exports = User
