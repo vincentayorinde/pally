3b7ab0d868512f3628a5d2a0d5fade030804db1b
vincentayorinde <vincentayorinde@yahoo.com>
2023-03-15 22:35:16 +0000

    temporarily disable db connection

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ import app from './app.js'
 import db from './db/models/index.js'
 
 const port = process.env.PORT || 7001
-db.sequelize.sync().then((req) => {
     app.listen(port, () => {
         console.log(`live on port ${port} (^__^)`)
     })
-})
+    // Temporarily disable db connection
+// db.sequelize.sync().then((req) => {
+//     app.listen(port, () => {
+//         console.log(`live on port ${port} (^__^)`)
+//     })
+// })
