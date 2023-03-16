756f151d321ef4521bd7d6d17416b1c56edfcd19
vincentayorinde <vincentayorinde@yahoo.com>
2023-03-16 09:15:20 +0000

    fix package json

--- services/mail.js
+++ services/mail.js
@@ -1,20 +1,20 @@
 import * as dotenv from 'dotenv'
-import sgMail from '@sendgrid/mail'
-dotenv.config()
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-  to: 'vincent@bitnob.com', // Change to your recipient
-  from: 'vincent@vincenttechblog.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sgMail
-  .send(msg)
-  .then((data) => {
-    console.log('the data', data);
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+// import sgMail from '@sendgrid/mail'
+// dotenv.config()
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// const msg = {
+//   to: 'vincent@bitnob.com', // Change to your recipient
+//   from: 'vincent@vincenttechblog.com', // Change to your verified sender
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// }
+// sgMail
+//   .send(msg)
+//   .then((data) => {
+//     console.log('the data', data);
+//     console.log('Email sent')
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
