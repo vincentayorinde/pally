0b6bd6f48e9e785035c363aa126e68a7c750b2dd
vincentayorinde <vincentayorinde@yahoo.com>
2023-03-16 03:12:11 +0000

    update cron style

--- controllers/tasks/task.js
+++ controllers/tasks/task.js
@@ -1,6 +1,5 @@
 import util from '../../utils/util.js'
-import datataService from '../../services/datata/index.js'
-
+import { handleBTCJob, handleETHJob } from '../../tasks/price/index.js'
 const sendToSlack = async (req, res) => {
     try {
         const { channel, message } = req.body
@@ -10,4 +9,22 @@ const sendToSlack = async (req, res) => {
     }
 }
 
-export default { sendToSlack }
+const runBTCJob = async (req, res) => {
+    try {
+        handleBTCJob()
+        util.serverResponse(res, 200, true, { message: 'BTC job ran successfully' })
+    } catch (error) {
+        util.serverResponse(res, 500, false, error)
+    }
+}
+
+const runETHJob = async (req, res) => {
+    try {
+        handleETHJob()
+        util.serverResponse(res, 200, true, { message: 'ETH job ran successfully' })
+    } catch (error) {
+        util.serverResponse(res, 500, false, error)
+    }
+}
+
+export default { sendToSlack, runBTCJob, runETHJob }
